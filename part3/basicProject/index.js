7f5d802ecbd0e882d6709b49f33de2bbd77a6fb5
Spidey6162099 <141052710+Spidey6162099@users.noreply.github.com>
2024-09-25 06:12:52 +0530

    added validator based verification

--- part3/basicProject/index.js
+++ part3/basicProject/index.js
@@ -9,7 +9,7 @@ const app=express();
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 app.get('/',(request,response)=>{
     response.send('<h1>Hello world</h1>')
@@ -21,7 +21,7 @@ app.get('/api/notes',(request,response)=>{
     })
 })
 
-app.get('/api/notes/:id',(request,response)=>{
+app.get('/api/notes/:id',(request,response,next)=>{
     Note.findById(request.params.id).then(noteById=>{
         if(noteById){
             response.json(noteById)
@@ -32,17 +32,20 @@ app.get('/api/notes/:id',(request,response)=>{
        
     })
     .catch(err=>{
-        response.status(500).end();
+        next(err)
     })
 })
 
-app.delete('/api/notes/:id',(request,response)=>{
-    Note.findById(request.params.id).then(noteById=>{
-        response.json(noteById)
+app.delete('/api/notes/:id',(request,response,next)=>{
+    Note.findByIdAndDelete(request.params.id).then(noteById=>{
+        response.status(204).end()
+    })
+    .catch(err=>{
+        next(err)
     })
 })
 
-app.post('/api/notes',(request,response)=>{
+app.post('/api/notes',(request,response,next)=>{
 
     const body=request.body
 
@@ -61,9 +64,41 @@ app.post('/api/notes',(request,response)=>{
     note.save().then(result=>{
             response.json(result)
     })
+    .catch(err=>{
+        next(err)
+    })
 
 
 })
+
+app.put('/api/notes/:id',(request,response,next)=>{
+    let id=request.params.id;
+    console.log(id)
+    const body=request.body;
+    console.log(body)
+
+    const note= {
+        "content":body.content,
+        "important":body.important
+    }
+
+    Note.findByIdAndUpdate(id,note,{new:true,runValidators:true}).then(result=>{
+        response.json(result)
+    })
+})
+
+const errorHandler=(error,request,response,next)=>{
+    console.log(error.message)
+
+    if(error.name=='CastError'){
+        response.status(400).send({"error":"malformed id"})
+    }
+    else if(error.name=="ValidationError"){
+        return response.status(400).json({error:error.message})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT=process.env.PORT||3001
 app.listen(PORT,()=>{
