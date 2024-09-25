9ef72c34b691410153290b733e6954b5646ba63c
Spidey6162099 <141052710+Spidey6162099@users.noreply.github.com>
2024-09-25 07:15:47 +0530

    fixed git issues

--- part2/PhoneBook/src/components/Person.jsx
+++
@@ -1,9 +0,0 @@
-const Person=({selectedPerson,handleDelete})=>{
-  // console.log(selectedPerson)
-    return (
-    <>
-    {selectedPerson.map((person)=><div key={person.id}>{person.name}  {person.number}  <button  onClick={()=>{handleDelete(person.id)}}>delete</button></div>)}
-  </>
-    )
-  }
-  export default Person
