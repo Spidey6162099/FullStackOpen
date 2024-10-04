89266f829cdb71acc89e743ba7cec91ea9c85295
Spidey6162099 <141052710+Spidey6162099@users.noreply.github.com>
2024-10-04 11:06:52 +0530

    port setup

--- part4/BlogProject/index.js
+++ part4/BlogProject/index.js
@@ -1,9 +1,10 @@
 const app=require('./app')
 const logger=require('./utils/logger')
+const config=require('./utils/config')
 
-const PORT=3003
-app.listen(PORT , ()=>{
-    logger.info('running on port '+PORT)
+
+app.listen(config.PORT , ()=>{
+    logger.info('running on port '+config.PORT)
 })
 
 
